57bce73698e8a514dc7b9cc5f266c675c88adaf3
faisalahammed044274 <setu2014@gmail.com>
2023-01-10 03:45:30 +0600

    Error Message

--- resources/views/hompage.blade.php
+++ resources/views/hompage.blade.php
@@ -11,21 +11,33 @@
             <div class="form-group">
               <label for="username-register" class="text-muted mb-1"><small>Username</small></label>
               <input name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+            @error('username')
+              <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
+            @enderror
             </div>
 
             <div class="form-group">
               <label for="email-register" class="text-muted mb-1"><small>Email</small></label>
               <input name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+              @error('email')
+              <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
+            @enderror
             </div>
 
             <div class="form-group">
               <label for="password-register" class="text-muted mb-1"><small>Password</small></label>
               <input name="password" id="password-register" class="form-control" type="password" placeholder="Create a password" />
+              @error('password')
+              <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
+            @enderror
             </div>
 
             <div class="form-group">
               <label for="password-register-confirm" class="text-muted mb-1"><small>Confirm Password</small></label>
               <input name="password_confirmation" id="password-register-confirm" class="form-control" type="password" placeholder="Confirm password" />
+              @error('password_confirmation')
+              <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
+            @enderror
             </div>
 
             <button type="submit" class="py-3 mt-4 btn btn-lg btn-success btn-block">Sign up for OurApp</button>
