2231ff84f8d56a85777d0cf0d5f3f9f41d76370b
faisalahammed044274 <setu2014@gmail.com>
2023-01-10 03:50:19 +0600

    remember previous info

--- resources/views/hompage.blade.php
+++ resources/views/hompage.blade.php
@@ -10,7 +10,7 @@
             @csrf
             <div class="form-group">
               <label for="username-register" class="text-muted mb-1"><small>Username</small></label>
-              <input name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
+              <input value="{{ old('username') }}" name="username" id="username-register" class="form-control" type="text" placeholder="Pick a username" autocomplete="off" />
             @error('username')
               <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
             @enderror
@@ -18,7 +18,7 @@
 
             <div class="form-group">
               <label for="email-register" class="text-muted mb-1"><small>Email</small></label>
-              <input name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
+              <input value="{{ old('email') }}" name="email" id="email-register" class="form-control" type="text" placeholder="you@example.com" autocomplete="off" />
               @error('email')
               <p class="m-0 small alert alert-danger shadow-sm"> {{ $message }}</p>
             @enderror
