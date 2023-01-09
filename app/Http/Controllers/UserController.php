1f05542800fcb33529fad698681e202004fd5d67
faisalahammed044274 <setu2014@gmail.com>
2023-01-09 16:40:51 +0600

    simple crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
 
-    public function register()
+    public function register(Request $request)
     {
+        $incomingFields = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        User::create($incomingFields);
+
         return "This is register function calling";
     }
 }
